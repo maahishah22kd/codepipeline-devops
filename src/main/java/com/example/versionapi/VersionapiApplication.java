5922151bf80f814a0913279b26c2ace09d214c81
Maahi <maahi.shah@kickdrumtech.com>
2026-02-27 11:41:46 +0530

    Initial clean structure

---
+++ src/main/java/com/example/versionapi/VersionapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.versionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VersionapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/versionapi/VersionapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.versionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
